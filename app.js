ef386f38709abab828daf9996310bfd0c966c767
TheBigH50 <HFangmeier@outlook.com>
2022-08-18 14:00:18 -0500

    Exe1 final

--- app.js
+++ app.js
@@ -7,3 +7,39 @@ console.log("EXERCISE 1:\n==========\n");
 console.log("EXERCISE 2:\n==========\n");
 
 
+//Exe1
+/*
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+
+function arraySum() {
+    let sum = 0;
+
+    for(let num of numbers) {
+        console.log(`sum = ${sum} = ${num}`)
+        sum += num;
+    }
+    
+    console.log(`Your sum is ${sum}`)
+}
+
+arraySum()
+*/
+
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+
+function arraySum (arr) {
+    let sum = 0;
+
+    arr.forEach((num) => {
+        sum += num; 
+        
+        //for (let index = 0; index < arr.length; index++) {
+            //sum += arr[index];
+    });
+    
+    console.log(sum);
+    
+};
+
+arraySum(numbers)
+
