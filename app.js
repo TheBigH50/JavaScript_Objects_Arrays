5711148a65d52f823622aa1c3a9bfd03b48986a5
TheBigH50 <HFangmeier@outlook.com>
2023-01-10 09:58:42 -0600

    finish exe4 parse csv

--- app.js
+++ app.js
@@ -62,7 +62,6 @@ let sentence = "The quick brown fox jumps over the lazy dog";
 
 // function to reverse a string
 function reverseStr(str) {
-
   let reversedStr = "";
 
   for (let i = 0; i < str.length; i++) {
@@ -76,13 +75,12 @@ function reverseStr(str) {
 // reverse each word in a sentence
 
 function reverseWordsFromScentence(sentence) {
-
   let words = sentence.split(" ");
 
   let reversedWords = words.map(function (word) {
-return reverseStr(word);
+    return reverseStr(word);
   });
-  
+
   return reversedWords.join(" ");
 }
 
@@ -91,5 +89,20 @@ console.log(reverseWordsFromScentence(sentence));
 //Exe4
 let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
 
+let result = [];
 let dataSplit = csvData.split("\n");
+let headers = dataSplit.slice(0, 1);
+let splitHead = headers[0].split(",");
+let content = dataSplit.slice(1);
 
+let splitContent = content.map((a) => {
+  return a.split(",");
+});
+
+for (let i = 0; i < content.length; i++) {
+  result.push({
+    [splitHead[0]]: splitContent[i][0],
+    [splitHead[1]]: splitContent[i][1],
+  });
+}
+console.log("An array of objects parsed from csv:", result);
